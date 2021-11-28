17baaf2d124a4e3c2dbc0520b9c95d251ac2d2a8
Ariel Parra <darkaico@gmail.com>
2021-11-28 12:55:04 +0100

    chore: Adding more tests, organizing conftest

--- tests/strava/test_api.py
+++ tests/strava/test_api.py
@@ -1,51 +1,6 @@
 import pytest
 import requests
 
-from strava_extensions.strava.api import StravaAPI
-from tests.conftest import MockResponse
-
-
-@pytest.fixture
-def strava_api(monkeypatch, mock_redis):
-    def mock_access_token(self):
-        return "access-token"
-
-    monkeypatch.setattr(StravaAPI, "get_access_token", mock_access_token)
-
-    return StravaAPI()
-
-
-@pytest.fixture
-def api_empty_result(monkeypatch):
-    def mockreturn(url, params=None):
-        return MockResponse(200, [])
-
-    monkeypatch.setattr(requests, "get", mockreturn)
-
-
-@pytest.fixture
-def api_with_activities(monkeypatch, json_club_activities):
-    def mockreturn(url, params=None):
-        return MockResponse(200, json_club_activities)
-
-    monkeypatch.setattr(requests, "get", mockreturn)
-
-
-@pytest.fixture
-def api_with_invalid_activities(monkeypatch, json_invalid_club_activities):
-    def mockreturn(url, params=None):
-        return MockResponse(200, json_invalid_club_activities)
-
-    monkeypatch.setattr(requests, "get", mockreturn)
-
-
-@pytest.fixture
-def api_http_error(monkeypatch):
-    def mockreturn(url, params=None):
-        return MockResponse(400)
-
-    monkeypatch.setattr(requests, "get", mockreturn)
-
 
 def test_get_club_activities_empty(strava_api, api_empty_result):
 
